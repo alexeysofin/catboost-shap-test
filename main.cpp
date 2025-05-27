4a573c98b17caa9268fca763bbbc828edbaab951
Aliaksei Sofin <aliaksei.sofin@intuitionmachines.com>
2025-05-27 13:53:07 +0200

    cleanup

--- main.cpp
+++ main.cpp
@@ -23,38 +23,7 @@ int main() {
 
   printf("result: %f", result[0]);
 
-  //   auto floats = std::vector<float>{1.1f, 2.2f};
-
-  //   auto d = floats.data();
-  //   auto p = d->data();
-  //   const float **p1 = &p;
-
-  // static const float data[1][2] = {{1.0f, 2.0f}};
-
-  // const float *ptrs[2];
-  // for (int i = 0; i < 2; ++i) {
-  //   ptrs[i] = data[i];
-  // }
-
-  // const float **cppStylePtr = ptrs;
-
-  // auto floats = std::vector<float>{1.1f, 2.2f};
-  // const float *p = floats.data();
-  // auto p2 = &p;
-  // //   auto p1 = floats.data();
-  // //   const float ** p2 = &floats[0].;
-
-  // auto strings = std::vector<const char *>{"hello"};
-
-  // auto p3 = strings.data();
-  // const char ***p4 = &p3;
-  //   auto p4 = &p3;
-  //   auto p5 = &p4;
-
   auto val = GetShapValues(modelHandle);
-  // std::cout << "val: " << val;
-
-  // auto m = ModelCalcerCreate();
 
   return 0;
 }
